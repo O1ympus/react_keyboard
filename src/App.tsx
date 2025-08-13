5fd951c27e1cb062fd5d445a6c8f676d07ae3ccb
keygen_exe <matkovskiyf3vlad@gmail.com>
2025-08-13 12:39:09 +0300

    sol

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,44 @@
 import React from 'react';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <p className="App__message">The last pressed key is [Enter]</p>
-  </div>
-);
+interface State {
+  pressedKey: string;
+  message: string;
+}
+
+export class App extends React.Component {
+  state: Readonly<State> = {
+    pressedKey: '',
+    message: '',
+  };
+
+  handleKeyPressing = (event: KeyboardEvent) => {
+    this.setState({ pressedKey: event.key });
+  };
+
+  componentDidMount() {
+    this.setState({ message: 'Nothing was pressed yet' });
+    document.addEventListener('keyup', this.handleKeyPressing);
+  }
+
+  componentDidUpdate(_prevProps: {}, prevState: State) {
+    if (prevState.pressedKey !== this.state.pressedKey) {
+      this.setState({
+        message: `The last pressed key is [${this.state.pressedKey}]`,
+      });
+    }
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keyup', this.handleKeyPressing);
+  }
+
+  render() {
+    const { message } = this.state;
+
+    return (
+      <div className="App">
+        <p className="App__message">{message}</p>
+      </div>
+    );
+  }
+}
