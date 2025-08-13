66360b6621981248c5e29e6fbc374f47397f9a5c
keygen_exe <matkovskiyf3vlad@gmail.com>
2025-08-13 13:14:40 +0300

    sol

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,6 @@ export class App extends React.Component {
   };
 
   componentDidMount() {
-    this.setState({ message: 'Nothing was pressed yet' });
     document.addEventListener('keyup', this.handleKeyPressing);
   }
 
